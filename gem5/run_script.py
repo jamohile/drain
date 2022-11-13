53d4c38d9939bff78033fa861b0089da84946c9c
Jay Mohile <jamohile@gmail.com>
2022-11-13 05:02:57 -0500

    refactor: major cleanups to run_script

--- gem5/run_script.py
+++ gem5/run_script.py
@@ -1,23 +1,27 @@
+from cmath import exp
+from logging import root
 import os
+from struct import pack
 import subprocess
+import json
 
-# import pdb; pdb.set_trace()
-# first compile then run
 binary = 'build/Garnet_standalone/gem5.opt'
-os.system("scons -j15 {}".format(binary))
 
 class RoutingAlgorithm:
+	"""
+	Abstraction layer over gem5-level routing algorithms.
+	These consist of a key, which the simulator uses, and a human-readable name.
+	"""
 	def __init__(self, key, name):
 		self.key = key
 		self.name = name
 
-class RoutingAlgorithms:
-	ADAPT_RAND_ = RoutingAlgorithm(0, "ADAPT_RAND_")
-	UP_DN_ = RoutingAlgorithm(1, "UP_DN_")
-	Escape_VC_UP_DN_ = RoutingAlgorithm(2, "Escape_VC_UP_DN_")
-
-# A single network topology configuration.
 class NetworkConfiguration:
+	"""
+	A single network topology configuration.
+	This contains all settings that are specific to a single network, i.e hardware-level in real life.
+	"""
+	
 	def __init__(self, num_cores, num_rows, mesh_config, spin_config, virtual_channels, routing_algorithm, spin_freq):
 		self.num_cores = num_cores
 		self.num_rows = num_rows
@@ -27,37 +31,24 @@ class NetworkConfiguration:
 		self.routing_algorithm = routing_algorithm
 		self.spin_freq = spin_freq
 
-network_configurations = [
-	NetworkConfiguration(
-		num_cores=64,
-		num_rows=8,
-		mesh_config="64_nodes-connectivity_matrix_0-links_removed_0.txt",
-		spin_config="spin_configs/SR_64_nodes-connectivity_matrix_0-links_removed_0.txt",
-		routing_algorithm=RoutingAlgorithms.ADAPT_RAND_,
-		virtual_channels=4,
-		spin_freq=1024
-
-	),
-
-	NetworkConfiguration(
-		num_cores=256,
-    num_rows=16,
-		mesh_config="256_nodes-connectivity_matrix_0-links_removed_0.txt",
-		spin_config="spin_configs/256_nodes-connectivity_matrix_0-links_removed_0.txt",
-		routing_algorithm=RoutingAlgorithms.ADAPT_RAND_,
-		virtual_channels=4,
-		spin_freq=1024
-	)
-]
-
-benchmarks=[ "bit_rotation", "shuffle", "transpose" ]
 
 class Measurement:
+	"""
+	A single experimental measured latency, given some injection rate.
+	"""
 	def __init__(self, injection_rate, packet_latency):
 		self.injection_rate = injection_rate
 		self.packet_latency = packet_latency
+	
+	def toDict(self):
+		return {"injection_rate": self.injection_rate, "packet_latency": self.packet_latency}
 
-class Result:
+class Experiment:
+	"""
+	An experiment consists of a single hardware configuration, paired alongside a single software configuration.
+	While running an experiment, packet latencies are collection across increasing injection rates.
+	"""
+
 	def __init__(self, network_config, benchmark):
 		self.network_config = network_config
 		self.benchmark = benchmark
@@ -72,85 +63,113 @@ class Result:
 	def add_packet_latency(self, injection_rate, packet_latency):
 		self.measurements.append(Measurement(injection_rate, packet_latency))
 
-out_dir = './results'
-cycles = 100000
+	def run(self, maximum_packet_latency, cycles, root_output_dir):
+			injection_rate = 0
 
-os.system('rm -rf ./results')
-os.system('mkdir results')
+			while self.last_packet_latency() < maximum_packet_latency:
+				injection_rate += 0.02
 
-def get_output_dir(network_config, benchmark, injection_rate):
-	path = [
-		out_dir,
-		"%s" % network_config.num_cores,
-		network_config.routing_algorithm.name,
-		benchmark.upper(),
-		"freq-%d" 	% network_config.spin_freq,
-		"vc-%d" 		% network_config.virtual_channels,
-		"inj-%d" 		% injection_rate
-	]
+				# Control flags for Gem5.
+				flags = [
+					"--topology=irregularMesh_XY",
+					"--network=garnet2.0",
+					"--router-latency=1",
+					"--spin=1",
+					"--spin-mult=1",
+					"--uTurn-crossbar=1",
+					"--inj-vnet=0",
+					"--synthetic=" 						+ self.benchmark,
+					"--spin-file=" 						+ self.network_config.spin_config,
+					"--conf-file=" 						+ self.network_config.mesh_config,
+					"--sim-cycles=%d" 				% cycles,
+					"--num-cpus=%d" 					% self.network_config.num_cores,
+					"--num-dirs=%d"  					% self.network_config.num_cores,
+					"--mesh-rows=%d"  				% self.network_config.num_rows,
+					"--spin-freq=%d" 					% self.network_config.spin_freq,
+					"--vcs-per-vnet=%d" 			% self.network_config.virtual_channels,
+					"--injectionrate=%1.2f"		% injection_rate,
+					"--routing-algorithm=%d" 	% self.network_config.routing_algorithm.key,
+				]
 
-	return "/".join(path)
+				output_dir = os.path.join(
+					root_output_dir,
+					"%s" % self.network_config.num_cores,
+					self.network_config.routing_algorithm.name,
+					self.benchmark.upper(),
+					"freq-%d" 	% self.network_config.spin_freq,
+					"vc-%d" 		% self.network_config.virtual_channels,
+					"inj-%d" 		% injection_rate
+				)
 
-results = []
+				# Run simulator with provided configuration.
+				subprocess.call([binary, "-d", output_dir, "configs/example/garnet_synth_traffic.py"] + flags)
 
-for network_config in network_configurations:
-	for benchmark in benchmarks:
-		print ("cores: {2:d} b: {0:s} vc-{1:d}".format(benchmark.upper(), network_config.virtual_channels, network_config.num_cores))
-		injection_rate = 0.02
+				# Scan through the simulator's output, to specificially find the average flit latency.
+				_, packet_latency = subprocess.check_output([
+					"grep", "system.ruby.network.average_flit_latency", os.path.join(output_dir, "stats.txt"),
+				]).split()
 
-		result = Result(network_config, benchmark)
+				self.add_packet_latency(injection_rate, float(packet_latency))
 
-		while result.last_packet_latency() < 200.0:
-			# Control flags for Gem5.
-			flags = " ".join([
-				"--topology=irregularMesh_XY",
-				"--network=garnet2.0",
-				"--router-latency=1",
-				"--spin=1",
-				"--spin-mult=1",
-				"--uTurn-crossbar=1",
-				"--inj-vnet=0",
-				"--synthetic=" 						+ benchmark,
-				"--spin-file=" 						+ network_config.spin_config,
-				"--conf-file=" 						+ network_config.mesh_config,
-				"--sim-cycles=%d" 				% cycles,
-				"--num-cpus=%d" 					% network_config.num_cores,
-				"--num-dirs=%d"  					% network_config.num_cores,
-				"--mesh-rows=%d"  				% network_config.num_rows,
-				"--spin-freq=%d" 					% network_config.spin_freq,
-				"--vcs-per-vnet=%d" 			% network_config.virtual_channels,
-				"--injectionrate=%1.2f"		% injection_rate,
-				"--routing-algorithm=%d" 	% network_config.routing_algorithm.key,
-			])
+	def toDict(self):
+		return {
+			"cores": self.network_config.num_cores,
+			"benchmark": self.benchmark.upper(),
+			"vc": self.network_config.virtual_channels,
 
-			output_dir = get_output_dir(network_config, benchmark, injection_rate)
+			"measurements": [m.toDict() for m in self.measurements]
+		}
 
-			command = " ".join([
-				binary,
-				# Output
-				"-d", output_dir,
-				# Input traffic
-				"configs/example/garnet_synth_traffic.py",
-				flags
-			])
+routing_algorithms = {
+	"ADAPT_RAND_": RoutingAlgorithm(0, "ADAPT_RAND_"),
+	"UP_DN_": RoutingAlgorithm(1, "UP_DN_"),
+	"Escape_VC_UP_DN_": RoutingAlgorithm(2, "Escape_VC_UP_DN_"),
+}
 
-			os.system(command)
+network_configurations = [
+	NetworkConfiguration(
+		num_cores=64,
+		num_rows=8,
+		mesh_config="64_nodes-connectivity_matrix_0-links_removed_0.txt",
+		spin_config="spin_configs/SR_64_nodes-connectivity_matrix_0-links_removed_0.txt",
+		routing_algorithm=routing_algorithms["ADAPT_RAND_"],
+		virtual_channels=4,
+		spin_freq=1024
 
-			packet_latency = subprocess.check_output("grep -nri average_flit_latency  {0:s}  | sed 's/.*system.ruby.network.average_flit_latency\s*//'".format(output_dir), shell=True)
-			result.add_packet_latency(injection_rate, packet_latency)
+	),
 
-			injection_rate+=0.02
+	NetworkConfiguration(
+		num_cores=256,
+    num_rows=16,
+		mesh_config="256_nodes-connectivity_matrix_0-links_removed_0.txt",
+		spin_config="spin_configs/SR_256_nodes-connectivity_matrix_0-links_removed_0.txt",
+		routing_algorithm=routing_algorithms["ADAPT_RAND_"],
+		virtual_channels=4,
+		spin_freq=1024
+	)
+]
 
-# Print all results.
-for result in results:
-	print(" ".join([
-		"cores: %d" 	% result.network_config.num_cores,
-		"benchmark: " + result.benchmark.upper(),
-		"vc-%d" 			% result.network_config.virtual_channels
-	]))
+benchmarks=[ "bit_rotation", "shuffle", "transpose" ]
 
-	for measurement in result.measurements:
-		print(" ".join([
-			"injection_rate: %1.2f" % measurement.injection_rate,
-			"packet_latency: %f" % measurement.packet_latency,
-		]))
+def main():
+	# Build simulator.
+	os.system("scons -j15 {}".format(binary))
+
+	# Clean up any leftover outputs.
+	subprocess.call('rm -rf ./results', shell=True)
+	subprocess.call('mkdir results', shell=True)
+
+	# Prepare experiments.
+	experiments = []
+	for network_config in network_configurations:
+		for benchmark in benchmarks:
+			experiments.append(Experiment(network_config, benchmark))
+	
+	# Run all experiments, then print results.
+	for experiment in experiments:
+		experiment.run(maximum_packet_latency=200.0, cycles=1e5, root_output_dir="./results")
+
+	for experiment in experiments:
+		print(json.dumps(experiment.toDict(), indent=2))
+
+main()
